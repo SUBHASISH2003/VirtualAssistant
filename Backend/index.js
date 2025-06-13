9efda81da3d59a917c334194add4c50ec95cf28b
Subhasish Pramanik <pramaniksubhasish28@gmail.com>
2025-06-13 22:39:07 +0530

    fixing bugs

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,6 @@ import { geminiResponse } from './src/service/GeminiRespose.js'
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }
 
 const app = express()
@@ -27,6 +25,9 @@ app.use(express.urlencoded({ extended: true }))
 
 connectDB()
 
+app.get("/api/test", (req, res) => {
+  res.json({ success: true, message: "Backend is working fine." });
+});
 
 
 app.use('/api/auth',authRouter)
